6f29b0ad6c2bb3d60044e39674c577e146b1d2f5
igor2519 <ig.klipkov@gmail.com>
2025-06-28 20:31:35 +0300

    feat: create navbar and good card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,32 @@
+import GoodCard from "@/features/good-card/GoodCard";
+import NavBar from "@/shared/ui/NavBar";
+import { Box, Typography } from "@mui/material";
+
 export default function Home() {
   return (
-    <div>
-    </div>
+    <Box sx={{ height: "fit-content" }}>
+      <Box sx={{ height: "100vh", width: "100vw" }}>
+        <NavBar />
+        <Box
+          component="section"
+          sx={{
+            background: "#FF69B4",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <Typography variant="h2" sx={{ fontWeight: 500, color: "white" }}>
+            MENSA CANDLES
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box sx={{ height: "100vh", width: "100vw" }}>
+        <GoodCard />
+      </Box>
+    </Box>
   );
 }
